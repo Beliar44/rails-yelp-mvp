53f5695dc21f14038d225bdde85c7988a2d26996
devautour julien <warnacraft@hotmail.fr>
2020-11-18 15:12:48 +0100

    ajout des routes et des controller + data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "start Seed"
+
+Restaurant.create(name: 'Le petit françois',  address: 'Nantes',  phone_number: '06 22 22 22 22', category: 'french' )
+Restaurant.create(name: 'chez Bob',           address: 'Reze',    phone_number: '06 00 12 22 22', category: 'french' )
+Restaurant.create(name: 'McDo',               address: 'Nantes',  phone_number: '06 33 33 33 33 ', category: 'french'  )
+Restaurant.create(name: 'Cop en pattes',      address: 'La Rochelle', phone_number: '06 44 44 44 44', category: 'french')
+Restaurant.create(name: 'Les Sardines',       address: 'Brest', phone_number: '06 55 55 55 55', category: 'french' )
+
+
+puts "finish !!!!"
